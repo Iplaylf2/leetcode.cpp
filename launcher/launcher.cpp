fce6f5b4081591dfdbec29c75c54e40a07300dce
i-play-lf2 <914637118@qq.com>
2022-01-22 23:17:02 +0800

    100%

--- launcher/launcher.cpp
+++ launcher/launcher.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include "cout-support.cpp"
-#include <vector>
-#include "1-two-sum/normal.cpp"
+#include "3-longest-substring-without-repeating-characters/normal.cpp"
 
 using std::cout;
 
 int main(int, char **)
 {
-    vector<int> input{2, 7, 11, 15};
-    cout << Solution().twoSum(input, 9);
+    cout << Solution().lengthOfLongestSubstring("abba");
 }
--- solution/3-longest-substring-without-repeating-characters/normal.cpp
+++ solution/3-longest-substring-without-repeating-characters/normal.cpp
@@ -1,11 +1,50 @@
 #include <string>
+#include <unordered_map>
+#include <algorithm>
 
+using std::max;
 using std::string;
+using std::unordered_map;
 
 class Solution
 {
 public:
     int lengthOfLongestSubstring(string s)
     {
+        int end = s.size();
+        switch (end)
+        {
+        case 0:
+            return 0;
+        case 1:
+            return 1;
+        default:
+            break;
+        }
+
+        auto record = unordered_map<char, int>();
+        auto anchor = 0;
+
+        auto maximun = 0;
+
+        for (int i = 0; i != end; i++)
+        {
+            auto current = s[i];
+
+            auto last = record.find(current);
+            if (last == record.end())
+            {
+                record.emplace(current, i);
+            }
+            else
+            {
+                maximun = max(maximun, i - anchor);
+                anchor = max(anchor, last->second + 1);
+
+                last->second = i;
+            }
+        }
+
+        return max(maximun, end - anchor);
     }
 };
