2f350c02e5c7ccd8d57ccdefe6a0334be9b5f323
i-play-lf2 <914637118@qq.com>
2022-01-29 16:26:06 +0800

    improve

--- solution/6-zigzag-conversion/normal.cpp
+++ solution/6-zigzag-conversion/normal.cpp
@@ -21,27 +21,25 @@ public:
 
         auto result = string();
 
-        auto source = s.data();
-
         auto magic_nr = -1 + numRows;
         auto max_offset = 2 * magic_nr;
 
         for (auto x = 0; x < length; x += max_offset)
         {
-            result += source[x];
+            result += s[x];
         }
 
         for (auto r = 1; r < magic_nr; r++)
         {
             for (auto x = r, offset = max_offset - 2 * r; x < length; x += offset, offset = max_offset - offset)
             {
-                result += source[x];
+                result += s[x];
             }
         }
 
         for (auto x = magic_nr; x < length; x += max_offset)
         {
-            result += source[x];
+            result += s[x];
         }
 
         return result;
@@ -52,24 +50,18 @@ private:
     {
         int length = s.length();
 
-        auto r1 = string();
-        auto r2 = string();
-
-        auto source = s.data();
+        char result[length + 1];
 
         for (auto i = 0, end = (1 + length) / 2; i != end; i++)
         {
             auto x = 2 * i;
 
-            r1 += source[x];
-            r2 += source[1 + x];
+            result[i] = s[x];
+            result[end + i] = s[1 + x];
         }
 
-        if (length % 2 == 1)
-        {
-            r2.pop_back();
-        }
+        result[length] = '\0';
 
-        return r1 + r2;
+        return result;
     }
 };
