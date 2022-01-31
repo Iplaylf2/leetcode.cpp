492316be1f25767be23fb781381744af29e4c6df
i-play-lf2 <914637118@qq.com>
2022-01-31 12:37:13 +0800

    complete

---
+++ solution/9-palindrome-number/normal.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        if (x < 0)
+        {
+            return false;
+        }
+
+        if (x == 0)
+        {
+            return true;
+        }
+
+        auto right = x % 10;
+        if (right == 0)
+        {
+            return false;
+        }
+
+        x /= 10;
+
+        for (;
+             right < x;
+             right = 10 * right + x % 10, x /= 10)
+            ;
+
+        return right == x || (right / 10) == x;
+    }
+};
