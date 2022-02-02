bb1ea63ebec0a3feefc90c40db043ae6be0f5280
Iplaylf2 <914637118@qq.com>
2022-02-02 20:01:38 +0800

    improve solution-11(#16)

--- solution/11-container-with-most-water/normal.cpp
+++ solution/11-container-with-most-water/normal.cpp
@@ -1,7 +1,5 @@
 #include <vector>
-#include <functional>
 
-using std::function;
 using std::vector;
 
 class Solution
@@ -9,88 +7,37 @@ class Solution
 public:
     int maxArea(vector<int> &height)
     {
-        int max_area;
+        auto max_area = 0;
 
         auto left = 0;
         auto right = ((int)height.size()) - 1;
-
-        auto max_left_height = height[left];
-        auto max_right_height = height[right];
-
-        function<void()> moveToRight;
-        function<void()> moveToLeft;
-
-        moveToRight = [&]()
+        do
         {
-            for (left++; left != right; left++)
+            auto wide = right - left;
+
+            auto left_height = height[left];
+            auto right_height = height[right];
+            if (left_height < right_height)
             {
-                auto left_height = height[left];
-                if (max_left_height < left_height)
+                auto area = wide * left_height;
+                if (max_area < area)
                 {
-                    max_left_height = left_height;
-                    auto wide = right - left;
-                    if (max_right_height < left_height)
-                    {
-                        auto area = wide * max_right_height;
-                        if (max_area < area)
-                        {
-                            max_area = area;
-                        }
-                        return moveToLeft();
-                    }
-                    else
-                    {
-                        auto area = wide * left_height;
-                        if (max_area < area)
-                        {
-                            max_area = area;
-                        }
-                    }
+                    max_area = area;
                 }
-            }
-        };
 
-        moveToLeft = [&]()
-        {
-            for (right--; left != right; right--)
+                left++;
+            }
+            else
             {
-                auto right_height = height[right];
-                if (max_right_height < right_height)
+                auto area = wide * right_height;
+                if (max_area < area)
                 {
-                    max_right_height = right_height;
-                    auto wide = right - left;
-                    if (max_left_height < right_height)
-                    {
-                        auto area = wide * max_left_height;
-                        if (max_area < area)
-                        {
-                            max_area = area;
-                        }
-                        return moveToRight();
-                    }
-                    else
-                    {
-                        auto area = wide * right_height;
-                        if (max_area < area)
-                        {
-                            max_area = area;
-                        }
-                    }
+                    max_area = area;
                 }
-            }
-        };
 
-        auto wide = right - left;
-        if (max_left_height < max_right_height)
-        {
-            max_area = wide * max_left_height;
-            moveToRight();
-        }
-        else
-        {
-            max_area = wide * max_right_height;
-            moveToLeft();
-        }
+                right--;
+            }
+        } while (left != right);
 
         return max_area;
     }
