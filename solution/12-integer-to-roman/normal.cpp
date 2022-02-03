1ed8048ae47eedc1dd35cf3e2f98259d2028cdb5
i-play-lf2 <914637118@qq.com>
2022-02-03 11:00:01 +0800

    complete solution-12

---
+++ solution/12-integer-to-roman/normal.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <array>
+
+using std::array;
+using std::pair;
+using std::string;
+
+class Solution
+{
+public:
+    string intToRoman(int num)
+    {
+        auto result = string();
+        for (auto &[atom, s] : dictionary)
+        {
+            while (atom <= num)
+            {
+                result += s;
+                num -= atom;
+            }
+
+            if (num == 0)
+            {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+private:
+    static const pair<int, const string> dictionary[13];
+};
+
+const pair<int, const string> Solution::dictionary[]{{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
