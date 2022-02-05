6d1c789fc82693e2d52de32fe7a86ef1b106714f
i-play-lf2 <914637118@qq.com>
2022-02-05 13:42:53 +0800

    complete special

--- launcher/debug.cpp
+++ launcher/debug.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include "15-3sum/normal.cpp"
+#include "15-3sum/special.cpp"
 #include "cout-support.cpp"
 
 using std::cout;
 
 int main(int, char **)
 {
-    auto foo = vector<int>({0});
+    auto foo = vector<int>({-1, 0, 1, 2, -1, -4});
     cout << Solution().threeSum(foo);
 }
