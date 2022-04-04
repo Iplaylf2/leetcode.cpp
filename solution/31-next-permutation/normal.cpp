d3a3ef3d967d5c3032766c58e586fb3d1527a8f9
i-play-lf2 <914637118@qq.com>
2022-04-04 12:50:15 +0000

    complete solution-31

---
+++ solution/31-next-permutation/normal.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <algorithm>
+
+using std::reverse;
+using std::swap;
+using std::vector;
+
+class Solution
+{
+public:
+    void nextPermutation(vector<int> &nums)
+    {
+        auto down_begin = 0;
+        auto last = nums[0];
+        int end = end = (int)nums.size();
+        for (int i = 1; end != i; i++)
+        {
+            auto current = nums[i];
+            if (last < current)
+            {
+                down_begin = i;
+            }
+            last = current;
+        }
+
+        reverse(nums.begin() + down_begin, nums.end());
+
+        auto head = down_begin - 1;
+        if (head < 0)
+        {
+            return;
+        }
+
+        auto head_value = nums[head];
+        while (nums[down_begin] <= head_value)
+        {
+            down_begin++;
+        }
+
+        swap(nums[head], nums[down_begin]);
+    }
+};
